ff55908642e87ddcbad25a6f74e09ca121c7f7ef
Vu Hoang Minh <vuhminh@gmail.com>
2018-12-26 15:06:26 +0700

    fix: deploy previous chanrges

--- src/theme/default-theme/typography/fontFamilies.ts
+++ src/theme/default-theme/typography/fontFamilies.ts
@@ -4,13 +4,13 @@ import { FontFamilies } from '../../ThemeInterface';
 
 export const fontFamilies: FontFamilies =
   Platform.OS === 'web'
-    ? // Use system font
+    ? // Use system font on the Web
       {
         heading: `"SF UI Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
         mono: `"SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", "Source Code Pro", monospace`,
         text: `"SF UI Text", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
       }
-    : // Use default font families by React Native by default
+    : // Use default font families by React Native by default. Setting it to undefined will use the default font
       {
         heading: undefined,
         mono: undefined,
