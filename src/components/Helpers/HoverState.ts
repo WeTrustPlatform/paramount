f4c37e727d2994699e7d1e6c9c98707791b91a87
Vu Hoang Minh <vuhminh@gmail.com>
2019-01-06 15:31:26 +0700

    feat: add Hoverable component

---
+++ src/components/Helpers/HoverState.ts
@@ -0,0 +1,38 @@
+import { canUseDOM } from 'exenv';
+
+let isEnabled = false;
+
+if (canUseDOM) {
+  /**
+   * Web browsers emulate mouse events (and hover states) after touch events.
+   * This code infers when the currently-in-use modality supports hover
+   * (including for multi-modality devices) and considers "hover" to be enabled
+   * if a mouse movement occurs more than 1 second after the last touch event.
+   * This threshold is long enough to account for longer delays between the
+   * browser firing touch and mouse events on low-powered devices.
+   */
+  const HOVER_THRESHOLD_MS = 1000;
+  let lastTouchTimestamp = 0;
+
+  const enableHover = () => {
+    if (isEnabled || Date.now() - lastTouchTimestamp < HOVER_THRESHOLD_MS) {
+      return;
+    }
+    isEnabled = true;
+  };
+
+  const disableHover = () => {
+    lastTouchTimestamp = Date.now();
+    if (isEnabled) {
+      isEnabled = false;
+    }
+  };
+
+  document.addEventListener('touchstart', disableHover, true);
+  document.addEventListener('touchmove', disableHover, true);
+  document.addEventListener('mousemove', enableHover, true);
+}
+
+export const isHoverEnabled = (): boolean => {
+  return isEnabled;
+};
--- src/components/Helpers/index.ts
+++ src/components/Helpers/index.ts
@@ -1,5 +1,10 @@
 export { default as ViewMeasure, ViewMeasureProps } from './ViewMeasure';
 export {
+  default as Hoverable,
+  HoverableProps,
+  HoverableChildrenProps,
+} from './Hoverable';
+export {
   default as RefMeasure,
   RefMeasureProps,
   Measurements,
