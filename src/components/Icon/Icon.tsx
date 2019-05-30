e4aa86540d83f00a289883aaa5eeacfb972432ac
Vu Hoang Minh <vuhminh@gmail.com>
2019-05-30 23:44:20 +0700

    feat(icon): replace expo icons with rn icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,5 +1,5 @@
-import { Feather } from '@expo/vector-icons';
 import * as React from 'react';
+import Feather from 'react-native-vector-icons/Feather';
 
 export interface IconProps {
   color?: string;
