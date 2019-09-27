c82521e7585d289ec5397f238674fe638602cb32
Vu Hoang Minh <vuhminh@gmail.com>
2019-09-27 15:21:29 +0700

    checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import { GestureResponderEvent, TouchableOpacity, View } from 'react-native';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+  ViewProps,
+} from 'react-native';
 
 import { ContainerShape, ControlSize, useTheme } from '../../theme';
-import { mergeStyles } from '../../utils/mergeStyles';
+import { isControlSize } from '../../utils/isControlSize';
+import { getOverrides, WithOverrides } from '../../utils/overrides';
 import { Icon } from '../Icon';
-import { GetCheckboxStyles, getCheckboxStyles } from './Checkbox.styles';
 
-export interface CheckboxProps {
+export interface CheckboxBaseProps {
   /**
    * The size of the checkbox.
    * @default "medium"
@@ -31,9 +36,6 @@ export interface CheckboxProps {
    */
   isInteractive?: boolean;
 
-  /** Label for screen readers */
-  accessibilityLabel?: string;
-
   /**
    * Shape of the checkbox.
    * @default "rounded"
@@ -43,46 +45,143 @@ export interface CheckboxProps {
   /**
    * Called when checkbox is pressed.
    */
-  onPress?: (e: GestureResponderEvent) => void;
-
-  /** Callback to get element styles. */
-  getStyles?: GetCheckboxStyles;
+  onValueChange?: (value: boolean) => void;
 
   /** Used to locate this view in end-to-end tests. */
   testID?: string;
 }
 
+export interface CheckboxOverrides {
+  Touchable: TouchableProps;
+  Check: CheckProps;
+}
+
+type CheckboxProps = WithOverrides<CheckboxBaseProps, CheckboxOverrides>;
+
 export const Checkbox = (props: CheckboxProps) => {
   const {
+    size = 'medium',
+    shape = 'rounded',
     value = false,
     isDisabled = false,
     isInteractive = true,
-    onPress,
-    getStyles,
+    onValueChange = () => {
+      return;
+    },
     testID,
-    accessibilityLabel,
+    overrides = {},
   } = props;
 
-  const theme = useTheme();
+  const [Touchable, touchableProps] = getOverrides(
+    StyledTouchable,
+    props,
+    overrides.Touchable,
+  );
+  const [Check, checkProps] = getOverrides(StyledCheck, props, overrides.Check);
 
-  const { touchableStyle, checkboxStyle, checkColor } = mergeStyles(
-    getCheckboxStyles,
-    getStyles,
-    theme.components.getCheckboxStyles,
-  )(props, theme);
+  return (
+    <Touchable
+      size={size}
+      shape={shape}
+      accessible={isInteractive}
+      isDisabled={isDisabled}
+      isInteractive={isInteractive}
+      testID={testID}
+      onPress={() => onValueChange(!value)}
+      {...touchableProps}
+    >
+      <Check
+        value={value}
+        size={size}
+        shape={shape}
+        isDisabled={isDisabled}
+        isInteractive={isInteractive}
+        {...checkProps}
+      />
+    </Touchable>
+  );
+};
+
+interface TouchableProps extends TouchableOpacityProps {
+  children?: React.ReactNode;
+  size: ControlSize | number;
+  isDisabled: boolean;
+  isInteractive: boolean;
+  shape: ContainerShape;
+}
+
+const StyledTouchable = (props: TouchableProps) => {
+  const {
+    size,
+    isDisabled,
+    isInteractive,
+    shape,
+    children,
+    style,
+    ...touchableProps
+  } = props;
+  const theme = useTheme();
 
   return (
     <TouchableOpacity
       accessible={isInteractive}
-      style={touchableStyle}
+      style={[theme.containerShapes[shape], style]}
       disabled={isDisabled || !isInteractive}
-      testID={testID}
-      onPress={onPress}
-      accessibilityLabel={accessibilityLabel}
+      {...touchableProps}
     >
-      <View style={checkboxStyle}>
-        {value ? <Icon name="check" size="xsmall" color={checkColor} /> : null}
-      </View>
+      {children}
     </TouchableOpacity>
+  );
+};
+
+interface CheckProps extends ViewProps {
+  size: ControlSize | number;
+  value: boolean;
+  isDisabled: boolean;
+  isInteractive: boolean;
+  shape: ContainerShape;
+}
+
+const StyledCheck = (props: CheckProps) => {
+  const { size, value, style, isDisabled, shape, ...viewProps } = props;
+  const theme = useTheme();
+
+  const sizeValue = isControlSize(size)
+    ? theme.controlHeights[size] - 16
+    : size;
+
+  return (
+    <View
+      style={[
+        {
+          alignItems: 'center',
+          backgroundColor: theme.colors.background.content,
+          borderColor: theme.colors.border.default,
+          borderWidth: 1,
+          height: sizeValue,
+          justifyContent: 'center',
+          width: sizeValue,
+          ...(value
+            ? {
+                backgroundColor: theme.colors.background.primaryDefault,
+                borderColor: 'transparent',
+              }
+            : {}),
+          ...(isDisabled
+            ? {
+                backgroundColor: theme.colors.background.greyDark,
+                borderColor: theme.colors.border.default,
+              }
+            : {}),
+          ...theme.containerShapes[shape],
+        },
+        style,
+      ]}
+      {...viewProps}
+    >
+      {value ? (
+        <Icon name="check" size="xsmall" color={theme.colors.text.white} />
+      ) : null}
+    </View>
   );
 };
