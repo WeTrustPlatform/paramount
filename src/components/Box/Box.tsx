1721b91d135298d6deaa5559f1a03e6865533fd7
Vu Hoang Minh <vuhminh@gmail.com>
2019-10-03 17:32:32 +0700

    fix: box space

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -12,9 +12,11 @@ import { getSpacing } from '../Spacing';
 
 export interface BoxProps extends ViewStyle {
   children?: React.ReactNode;
+  testID?: string;
 
   shape?: ContainerShape;
   backgroundColor?: BackgroundColor;
+
   space?: SpacingSize | number;
   spaceBottom?: SpacingSize | number;
   spaceEnd?: SpacingSize | number;
@@ -26,50 +28,76 @@ export interface BoxProps extends ViewStyle {
   spaceVertical?: SpacingSize | number;
 }
 
-const propToFn = {
-  backgroundColor: (color: BackgroundColor, theme: Theme) => {
+type BoxProp = keyof Omit<Omit<BoxProps, 'children'>, 'testID'>;
+
+const propToFn: Partial<
+  {
+    [key in BoxProp]: (
+      prop: NonNullable<BoxProps[key]>,
+      theme: Theme,
+    ) => ViewStyle;
+  }
+> = {
+  backgroundColor: (color: BackgroundColor | string, theme: Theme) => {
     // @ts-ignore
     if (theme.colors.background[color]) {
       // @ts-ignore
       return { backgroundColor: theme.colors.background[color] };
     }
-    return {
-      backgroundColor: color,
-    };
+
+    return { backgroundColor: color };
   },
-  elevation: (elevation: 0 | 1 | 2 | 3 | 4, theme: Theme) =>
-    theme.elevations[elevation],
+  elevation: (elevation: number, theme: Theme) => theme.elevations[elevation],
+
   shape: (shape: ContainerShape, theme: Theme) => theme.containerShapes[shape],
-  space: getSpacing,
-  spaceBottom: getSpacing,
-  spaceEnd: getSpacing,
-  spaceHorizontal: getSpacing,
-  spaceLeft: getSpacing,
-  spaceRight: getSpacing,
-  spaceStart: getSpacing,
-  spaceTop: getSpacing,
-  spaceVertical: getSpacing,
+
+  space: (size: SpacingSize | number, theme: Theme) => ({
+    padding: getSpacing(size, theme),
+  }),
+  spaceBottom: (size: SpacingSize | number, theme: Theme) => ({
+    paddingBottom: getSpacing(size, theme),
+  }),
+  spaceEnd: (size: SpacingSize | number, theme: Theme) => ({
+    paddingEnd: getSpacing(size, theme),
+  }),
+  spaceHorizontal: (size: SpacingSize | number, theme: Theme) => ({
+    paddingHorizontal: getSpacing(size, theme),
+  }),
+  spaceLeft: (size: SpacingSize | number, theme: Theme) => ({
+    paddingLeft: getSpacing(size, theme),
+  }),
+  spaceRight: (size: SpacingSize | number, theme: Theme) => ({
+    paddingRight: getSpacing(size, theme),
+  }),
+  spaceStart: (size: SpacingSize | number, theme: Theme) => ({
+    paddingStart: getSpacing(size, theme),
+  }),
+  spaceTop: (size: SpacingSize | number, theme: Theme) => ({
+    paddingTop: getSpacing(size, theme),
+  }),
+  spaceVertical: (size: SpacingSize | number, theme: Theme) => ({
+    paddingVertical: getSpacing(size, theme),
+  }),
 };
 
 export const Box = (props: BoxProps) => {
   const { children, testID, ...viewStyles } = props;
   const theme = useTheme();
   const transformedStyles = [];
-  const pureStyles = {};
+  const pureStyles: ViewStyle = {};
 
   for (const prop in viewStyles) {
     if (prop) {
-      // @ts-ignore
-      const styleFn = propToFn[prop];
+      const styleFn = propToFn[prop as BoxProp];
 
       if (styleFn) {
         // @ts-ignore
-        const style = styleFn(viewStyles[prop], theme);
+        const style = styleFn(viewStyles[prop as BoxProp], theme);
 
         transformedStyles.push(style);
       } else {
         // @ts-ignore
-        pureStyles[prop] = viewStyles[prop];
+        pureStyles[prop as keyof ViewStyle] = viewStyles[prop as BoxProp];
       }
     }
   }
